449255bce56f1716e92457442e1ab4186ea1edfe
Abhishek Duggal <abhishekduggal@Abhisheks-MacBook-Pro.local>
2018-07-13 10:35:10 -0500

    Initial Setup Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./Component/Dashboard/Dashboard";
+import Wizard from "./Component/Wizard/Wizard";
+import House from "./Component/House/House";
+import Header from "./Component/Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Header />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dashboard />
+        <Wizard />
+        <House />
       </div>
     );
   }
